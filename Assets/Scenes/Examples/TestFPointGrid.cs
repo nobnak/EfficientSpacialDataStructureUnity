6bd9b21bf3212144a0bb207bc6dc2031f3df953e
Nakata Nobuyuki <nobu.nakata@gmail.com>
2022-04-13 15:53:56 +0900

    add demo scene

---
+++ Assets/Scenes/Examples/TestFPointGrid.cs
@@ -0,0 +1,176 @@
+using EffSpace.Collections;
+using EffSpace.Extensions;
+using EffSpace.Models;
+using LLGraphicsUnity;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.Mathematics;
+using UnityEngine;
+
+namespace EffSpace.Examples {
+
+    public class TestFPointGrid : MonoBehaviour {
+
+		public Tuner tuner = new Tuner();
+		public Link link = new Link();
+
+		protected float2 screen;
+		protected FPointGrid grid;
+		protected int2 cellCount;
+		protected float2 cellSize;
+
+		protected float4x4 screenToWorld;
+		protected List<Particle> particleList;
+
+		protected GLMaterial gl;
+
+        private void OnEnable() {
+			gl = new GLMaterial();
+
+            var c = Camera.main;
+			screen = new float2(c.pixelWidth, c.pixelHeight);
+
+			var hSize = c.orthographicSize;
+			var aspect = c.aspect;
+			screenToWorld = float4x4.TRS(
+				new float3(-aspect * hSize, -hSize, 0f),
+				quaternion.identity,
+				new float3(2f * aspect * hSize / screen.x, 2f * hSize / screen.y, 1f)
+				);
+
+			FPointGridExt.RecommendGrid(screen, 1 << tuner.grid, out cellCount, out cellSize);
+			var fieldSize = cellCount * cellSize;
+			Debug.Log($"Screen: {screen}, Grid: n={cellCount}, field={fieldSize}");
+
+			grid = new FPointGrid(cellCount, cellSize, float2.zero);
+
+			var rand = Unity.Mathematics.Random.CreateFromIndex(31);
+			particleList = new List<Particle>();
+			for (var i = 0; i < tuner.count; i++) {
+				var p = new Particle();
+				var seed = rand.NextFloat2(float2.zero, screen);
+				
+				var go = Instantiate(link.fab);
+				go.transform.SetParent(transform);
+				go.transform.localPosition = math.transform(screenToWorld, new float3(seed, 0f));
+
+				var e = grid.Insert(i, seed);
+				if (e < 0)
+					Debug.LogWarning($"Position not on screen: {seed}");
+
+				p.id = i;
+				p.element = e;
+				p.go = go;
+				p.seed = seed;
+				p.pos = new float3(seed, 0f);
+				particleList.Add(p);
+			}
+
+			foreach (var p in particleList) p.go.SetActive(true);
+		}
+		private void OnDisable() {
+			for (var i = 0; i < particleList.Count; i++) {
+				var p = particleList[i];
+				Destroy(p.go);
+			}
+			particleList.Clear();
+
+			if (gl != null) {
+				gl.Dispose();
+				gl = null;
+			}
+		}
+		private void Update() {
+			var t = Time.time * tuner.freq;
+			var dt = Time.deltaTime * tuner.speed;
+			var counter = 0;
+			for (var i = 0; i < particleList.Count; i++) {
+				var p = particleList[i];
+				if (p.element >= 0) grid.Remove(p.element);
+
+				var pos = p.pos.xy;
+				pos += dt * screen * new float2(
+					noise.snoise(new float3(p.seed, t)),
+					noise.snoise(new float3(-p.seed, t)));
+				pos -= screen * math.floor(pos / screen);
+
+				p.pos = new float3(pos, p.pos.z);
+				p.go.transform.localPosition = math.transform(screenToWorld, p.pos);
+
+				p.element = grid.Insert(p.id, pos);
+				if (p.element >= 0) counter++;
+			}
+
+			var cdiff = particleList.Count - counter;
+			if (cdiff != 0) Debug.LogWarning($"Num particles outside of grid: {cdiff}");
+		}
+		private void OnRenderObject() {
+			var data = new GLProperty() {
+				Color = Color.white,
+				MainTex = null,
+				ZWriteMode = false,
+				ZTestMode = GLProperty.ZTestEnum.ALWAYS,
+			};
+			var qrange = 0.1f * screen.yy;
+			var search_limit_dist_sq = 2f * qrange.y * qrange.y;
+
+			using (new GLMatrixScope())
+			using (gl.GetScope(data)) {
+				GL.LoadIdentity();
+				GL.LoadPixelMatrix();
+
+				for (var i = 0; i < particleList.Count; i++) {
+					var p = particleList[i];
+					var pos = p.pos.xy;
+					var min_dist_sq = float.MaxValue;
+					var min_pos = float2.zero;
+					foreach (var e in grid.Query(pos - qrange, pos + qrange)) {
+						if (e == p.element) continue;
+
+						var eq = grid.grid.elements[e];
+						var q = particleList[eq.id];
+
+						var qpos = q.pos.xy;
+						var dist_sq = math.distancesq(qpos, pos);
+						if (dist_sq < min_dist_sq) {
+							min_dist_sq = dist_sq;
+							min_pos = qpos;
+						}
+					}
+					if (min_dist_sq > search_limit_dist_sq) continue;
+					var d = math.sqrt(min_dist_sq);
+					using (new GLModelViewScope(Matrix4x4.TRS(p.pos, quaternion.identity, Vector3.one))) {
+						GLTool.CircleOutlineVertices(0.5f * d, 20).DrawLineStrip();
+					}
+				}
+			}
+		}
+
+		[System.Serializable]
+        public class Tuner {
+            public int grid = 10;
+			public int count = 10;
+			public float speed = 0.1f;
+			public float freq = 0.1f;
+        }
+		[System.Serializable]
+		public class Link {
+			public GameObject fab;
+		}
+
+		[System.Serializable]
+		public class Particle {
+			public int id;
+			public int element;
+			public float2 seed;
+			public float3 pos;
+			public GameObject go;
+
+			public override string ToString() {
+				return $"{GetType().Name}: {id}/{element}, pos={pos}";
+			}
+		}
+    }
+
+}
